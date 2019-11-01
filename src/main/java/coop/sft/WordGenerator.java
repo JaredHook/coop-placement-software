7ac70fe2a707c2451ff1902ab4dccb3fb2429ae2
Jared Hook <jared_hook@hotmail.com>
2019-11-01 11:35:32 -0400

    build search and replace and template for word document

--- src/main/java/coop/sft/WordGenerator.java
+++ src/main/java/coop/sft/WordGenerator.java
@@ -1,5 +1,7 @@
 package coop.sft;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
@@ -35,26 +37,34 @@ public class WordGenerator {
     //Create Word
     public void createWord() throws IOException {
             //Blank Document
-            XWPFDocument document = new XWPFDocument();
-            //Write the Document in file system
+        XWPFDocument document = null;
+        try {
+            document = new XWPFDocument(OPCPackage.open("src/main/resources/coop.sft/coopSoftwareDoc.dotx"));
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        //Write the Document in file system
             FileOutputStream out = new FileOutputStream(
                     new File("createdWord" + ".docx"));
             //create Paragraph
-            XWPFParagraph docHeader = document.createParagraph();
-            docHeader.setAlignment(ParagraphAlignment.CENTER);
-            XWPFRun title = docHeader.createRun();
-            XWPFRun docTitle = docHeader.createRun();
-            title.setText("WILLIS COLLEGE " + "\n");
-            docTitle.setText("STUDENT COOP PLACEMENT STATUS");
-            // styling for text
-            title.setFontFamily("Arial");
-            title.setBold(true);
-            title.setFontSize(18);
-            docTitle.setFontFamily("Arial");
-            docTitle.setBold(true);
-            docTitle.setFontSize(14);
+            replaceText(document, "Student Name", "Jeannie");
+
             document.write(out);
 
+
+//        XWPFParagraph docHeader = document.createParagraph();
+//        docHeader.setAlignment(ParagraphAlignment.CENTER);
+//        XWPFRun title = docHeader.createRun();
+//        XWPFRun docTitle = docHeader.createRun();
+//        title.setText("WILLIS COLLEGE " + "\n");
+//            docTitle.setText("STUDENT COOP PLACEMENT STATUS");
+//            // styling for text
+//            title.setFontFamily("Arial");
+//            title.setBold(true);
+//            title.setFontSize(18);
+//            docTitle.setFontFamily("Arial");
+//            docTitle.setBold(true);
+//            docTitle.setFontSize(14);
 //            PrintFile pf = new PrintFile();
 //            try {
 //                pf.printFile(/*add document here and pass it to print method to be printed*/);
@@ -64,6 +74,21 @@ public class WordGenerator {
             //Close document
             out.close();
             System.out.println("createdWord" + ".docx" + " written successfully");
+        }
+
+        private static XWPFDocument replaceText(XWPFDocument doc, String findText, String replaceText) {
+
+            for (XWPFParagraph p : doc.getParagraphs()) {
+                List<XWPFRun> runs = p.getRuns();
+                for (XWPFRun r : runs) {
+                    String text = r.getText(0);
+                    if(text != null && text.contains(findText)) {
+                        text = text.replace(findText, replaceText);
+                        r.setText(text, 0);
+                    }
+                }
+            }
+            return doc;
         }
 
 }
