5f5aef6cc49b47d5076c00cc5cff1828745edebf
Jared Hook <jared_hook@hotmail.com>
2019-11-08 10:29:46 -0500

    Build logic to flatten paragraphs and print the result

--- src/main/java/coop/sft/WordGenerator.java
+++ src/main/java/coop/sft/WordGenerator.java
@@ -39,7 +39,7 @@ public class WordGenerator {
         //Blank Document
         XWPFDocument document = null;
         try {
-            document = new XWPFDocument(OPCPackage.open("src/main/resources/coop.sft/coopSoftwareDoc.dotx"));
+            document = new XWPFDocument(OPCPackage.open("src/main/resources/coop.sft/CoopTemplate.docx"));
         } catch (InvalidFormatException e) {
             e.printStackTrace();
         }
@@ -47,29 +47,82 @@ public class WordGenerator {
         File file = new File("createdWord.docx");
         FileOutputStream out = new FileOutputStream(file);
         //create Paragraph
-        replaceText(document, "StudentName", person.getfirstName() + " " + person.getlastName());
-        replaceText(document, "Course/Batch/Year", person.getcourse() + "/" + person.getbatch() + "/");
-        replaceText(document, "CoopHost", "");
-        replaceText(document, "StartDate", "");
-        replaceText(document, "EndDate", "");
-        replaceText(document, "ContractSignedOn", "");
-        replaceText(document, "Actual-End-Date", "");
-        replaceText(document, "WSIBOnFile", "");
-        replaceText(document, "InsuranceOnFile", "");
-        replaceText(document, "Evaluation1DueDate", "");
-        replaceText(document, "Evaluation1SubmittedOn", "");
-        replaceText(document, "Evaluation2DueDate", "");
-        replaceText(document, "Evaluation2SubmittedOn", "");
-        replaceText(document, "TimesheetDueDate", "");
-        replaceText(document, "TimesheetSubmittedOn", "");
-        replaceText(document, "WorkAddress", "");
-        replaceText(document, "ContactPerson", "");
-        replaceText(document, "ContactNumber", "");
-        replaceText(document, "FollowUps", "");
-        replaceText(document, "PassFailEntered", "");
-        replaceText(document, "AttendanceEntered", "");
-        replaceText(document, "OfficeManager", "");
-        replaceText(document, "PlacementCoordinator", "");
+        TextReplacer replacer = new TextReplacer("$Student", person.getfirstName() + " " + person.getlastName());
+        replacer.replace(document);
+        replacer.setSearchValue("$Course");
+        replacer.setReplacement("");
+        replacer.replace(document);
+        replacer.setSearchValue("$Batch");
+        replacer.replace(document);
+        replacer.setSearchValue("$Year");
+        replacer.replace(document);
+        replacer.setSearchValue("$Coop-Host");
+        replacer.replace(document);
+        replacer.setSearchValue("$Start-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$End-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$Contract-Signed-On");
+        replacer.replace(document);
+        replacer.setSearchValue("$Actual-End-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$WSIB-On-File");
+        replacer.replace(document);
+        replacer.setSearchValue("$Insurance-On-File");
+        replacer.replace(document);
+        replacer.setSearchValue("$Evaluation-1-Due-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$Evaluation-1-Submitted-On");
+        replacer.replace(document);
+        replacer.setSearchValue("$Evaluation-2-Due-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$Evaluation-2-Submitted-On");
+        replacer.replace(document);
+        replacer.setSearchValue("$Timesheet-Due-Date");
+        replacer.replace(document);
+        replacer.setSearchValue("$Timesheet-Submitted-On");
+        replacer.replace(document);
+        replacer.setSearchValue("$Work-Address");
+        replacer.replace(document);
+        replacer.setSearchValue("$Contact-Person");
+        replacer.replace(document);
+        replacer.setSearchValue("$Contact-Number");
+        replacer.replace(document);
+        replacer.setSearchValue("$Follow-ups");
+        replacer.replace(document);
+        replacer.setSearchValue("$Pass-Fail-Entered");
+        replacer.replace(document);
+        replacer.setSearchValue("$Attendance-Entered");
+        replacer.replace(document);
+        replacer.setSearchValue("$Office-Manager");
+        replacer.replace(document);
+        replacer.setSearchValue("$Placement-Coordinator");
+        replacer.replace(document);
+//        replaceText(document, "$Student", person.getfirstName() + " " + person.getlastName());
+//        replaceText(document, "$Course", "");
+//        replaceText(document, "$Batch",  "");
+//        replaceText(document, "$Year", "");
+//        replaceText(document, "$CoopHost", "");
+//        replaceText(document, "$Start-Date", "");
+//        replaceText(document, "$End-Date", "");
+//        replaceText(document, "$Contract-Signed-On", "");
+//        replaceText(document, "$Actual-End-Date", "");
+//        replaceText(document, "$WSIB-On-File", "");
+//        replaceText(document, "$Insurance-On-File", "");
+//        replaceText(document, "$Evaluation-1-Due-Date", "");
+//        replaceText(document, "$Evaluation-1-Submitted-On", "");
+//        replaceText(document, "$Evaluation-2-Due-Date", "");
+//        replaceText(document, "$Evaluation-2-Submitted-On", "");
+//        replaceText(document, "$Timesheet-Due-Date", "");
+//        replaceText(document, "$Timesheet-Submitted-On", "");
+//        replaceText(document, "$Work-Address", "");
+//        replaceText(document, "$Contact-Person", "");
+//        replaceText(document, "$Contact-Number", "");
+//        replaceText(document, "$Follow-ups", "");
+//        replaceText(document, "$Pass-Fail-Entered", "");
+//        replaceText(document, "$Attendance-Entered", "");
+//        replaceText(document, "$Office-Manager", "");
+//        replaceText(document, "$Placement-Coordinator", "");
 
         document.write(out);
 
@@ -88,30 +141,34 @@ public class WordGenerator {
 //            docTitle.setBold(true);
 //            docTitle.setFontSize(14);
 
-            PrintFile pf = new PrintFile();
-            try {
-                pf.printFile(file);
-            } catch (PrintException e) {
-                e.printStackTrace();
-            }
+
         //Close document
         out.close();
+
+        PrintFile pf = new PrintFile();
+        try {
+            pf.printFile(file);
+        } catch (PrintException e) {
+            e.printStackTrace();
+        }
         System.out.println("createdWord" + ".docx" + " written successfully");
     }
 
-    private static XWPFDocument replaceText(XWPFDocument doc, String findText, String replaceText) {
 
-        for (XWPFParagraph p : doc.getParagraphs()) {
-            List<XWPFRun> runs = p.getRuns();
-            for (XWPFRun r : runs) {
-                String text = r.getText(0);
-                if (text != null && text.contains(findText)) {
-                    text = text.replace(findText, replaceText);
-                    r.setText(text, 0);
-                }
-            }
-        }
-        return doc;
-    }
+
+//    private static XWPFDocument replaceText(XWPFDocument doc, String findText, String replaceText) {
+//
+//        for (XWPFParagraph p : doc.getParagraphs()) {
+//            List<XWPFRun> runs = p.getRuns();
+//            for (XWPFRun r : runs) {
+//                String text = r.getText(0);
+//                if (text != null && text.contains(findText)) {
+//                    text = text.replace(findText, replaceText);
+//                    r.setText(text, 0);
+//                }
+//            }
+//        }
+//        return doc;
+//    }
 
 }
