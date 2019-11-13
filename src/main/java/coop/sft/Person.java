907f122b613c196f5dc782704149193fa7d8c1bf
jp-2019 <jeannie.wesell@gmail.com>
2019-11-13 07:49:53 -0500

    Update person.java

--- src/main/java/coop/sft/Person.java
+++ src/main/java/coop/sft/Person.java
@@ -2,10 +2,9 @@ package coop.sft;
 
 public class Person {
 
-// TODO: add Start-Date, End-Date, Contract-Signed-On, Actual-End-Date, Evaluation-1-Due-Date, Evaluation-1-Submitted-On,
+// TODO: add Actual-End-Date, Evaluation-1-Due-Date, Evaluation-1-Submitted-On,
 // TODO: add Evaluation-2-Due-Date, Evaluation-2-Submitted-On, Timesheet-Due-Date, Timesheet-Submitted-On,
 // TODO: add Contact-Number, Follow-ups, Pass-Fail-Entered, Attendance-Entered, Office-Manager, Placement-Coordinator
-// TODO: what is ContactAdd variable set for in template? Is it Contact Address?
 // TODO: ensure all fields are accounted for
 
     private String firstName;
@@ -14,11 +13,14 @@ public class Person {
     private String batch;
     private String year;
     private String coopHost;
+    private String startDate;
+    private String endDate;
     private String projectName;
     private String coopPlacement;
     private String wsib;
     private String placement;
     private String insurance;
+    private String contractSignedOn;
     private String contactAdd;
     private String workAdd;
 
@@ -37,11 +39,14 @@ public class Person {
     }
     public void setYear(String year) { this.year = year; }
     public void setCoopHost(String coopHost) { this.coopHost = coopHost; }
+    public void setStartDate(String startDate) { this.startDate = startDate; }
+    public void setEndDate(String endDate) { this.endDate = endDate; }
     public void setProjectName(String projectName) { this.projectName = projectName; }
     public void setCoopPlacement(String coopPlacement) { this.coopPlacement = coopPlacement; }
     public void setWsib(String wsib) { this.wsib = wsib; }
     public void setPlacement(String placement) { this.placement = placement; }
     public void setInsurance(String insurance) { this.insurance = insurance; }
+    public void setContractSignedOn(String contractSignedOn){ this.contractSignedOn = contractSignedOn; }
     public void setContactAdd(String contactAdd) { this.contactAdd = contactAdd; }
     public void setWorkAdd(String workAdd) { this.workAdd = workAdd; }
 
@@ -58,11 +63,14 @@ public class Person {
     public String getBatch() { return batch; }
     public String getYear() { return year; }
     public String getCoopHost() { return coopHost; }
+    public String getStartDate() { return startDate; }
+    public String getEndDate() { return endDate; }
     public String getProjectName() { return projectName; }
     public String getCoopPlacement() { return coopPlacement; }
     public String getWsib() { return wsib; }
     public String getPlacement() { return placement; }
     public String getInsurance() { return insurance; }
+    public String getContractSignedOn() { return contractSignedOn; }
     public String getContactAdd() { return contactAdd; }
     public String getWorkAdd() { return workAdd; }
 
